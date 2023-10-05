83ae2be001051d6bb7af9c586eb8a41ccaf4408c
Daniel Rosenberg <drosen@google.com>
2023-10-05 15:53:49 -0700

    fuse-bpf: Add partial flock support
    This adds passthrough support for flock on fuse-bpf files. It does not
    give any control via a bpf filter. The flock will act as though it was
    taken on the lower file.
    
    Test: fuse_test -t32 (flock_test)
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: I95c77717fd731b19a4f9a9822bb5275c6a36215a

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1687,6 +1687,7 @@ static inline int fuse_bpf_access(int *out, struct inode *inode, int mask)
 
 #endif // CONFIG_FUSE_BPF
 
+int fuse_file_flock_backing(struct file *file, int cmd, struct file_lock *fl);
 ssize_t fuse_backing_mmap(struct file *file, struct vm_area_struct *vma);
 
 int fuse_handle_backing(struct fuse_bpf_entry *fbe, struct path *backing_path);
